7e608b1a1e6ffc6407ec03688d17135b8cddb47b
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-10-01 23:41:21 +0530

    Added studen add request

--- app/routes/students.py
+++ app/routes/students.py
@@ -1,7 +1,16 @@
-from flask import jsonify
-from app import app
+from flask import request, jsonify
+from app import app, db
 from app.models import Student, StudentSchema, StudentsSchema
-@app.route('/students')
-def getStudents():
+@app.route('/students', methods=['get'])
+def students_get():
     students = Student.query.all()
     return StudentsSchema.jsonify(students)
+
+@app.route('/students', methods=['post'])
+def students_post():
+    name = request.json['name']
+    roll_no = request.json['roll_no']
+    student = Student(name, roll_no)
+    db.session.add(student)
+    db.session.commit()
+    return StudentSchema.jsonify(student)
