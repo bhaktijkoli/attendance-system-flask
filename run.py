fbbdd11e4bdcc478d7743b6205b898d1ff4f9aa5
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-10-02 17:09:30 +0530

    Added zeroconf

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 from app import app, db
+import socket
+from zeroconf import ServiceInfo, Zeroconf
 
 if __name__ == '__main__':
+    # REGISTER ZEROCONF SERVICE
+    desc = {'path': '/~paulsm/'}
+    info = ServiceInfo("_http._tcp.local.",
+    "Attendance System._http._tcp.local.",
+    socket.inet_aton("127.0.0.1"), 80, 0, 0,
+    desc, "attendancesys.local.")
+
+    zeroconf = Zeroconf()
+    zeroconf.register_service(info)
+
+    # RUN APPLICATION
     app.run(host='0.0.0.0', debug=True)
