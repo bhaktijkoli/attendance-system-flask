1cbb9dc28de3bfda0e72b10f2a5b88307eb4c6b3
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-10-06 23:36:00 +0530

    Fix students

--- app/routes/students.py
+++ app/routes/students.py
@@ -2,6 +2,8 @@ from flask import request, jsonify
 from app import app, db
 from werkzeug.utils import secure_filename
 from app.models import Student, StudentSchema, StudentsSchema
+import numpy as np
+import face_recognition
 import secrets
 import os
 
